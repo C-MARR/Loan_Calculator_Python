ca350fec8fcda294cb03f320fec2cefb96740a48
Cameron <c-em@outlook.com>
2022-05-18 20:10:53 -0700

    Added Annuity payment

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,22 +1,33 @@
 import math
 
-loan_principal = int(input("Enter the loan principal: \n"))
 print("""What do you want to calculate?
-type "m" - for number of monthly payments,
-type "p" - for the monthly payment:""")
+type "n" for number of monthly payments,
+type "a" for annuity monthly payment amount,
+type "p" for loan principal:""")
 option = input()
-if option == "m":
+if option == "n":
+    principal = int(input("Enter the loan principal: \n"))
     monthly_payment = int(input("Enter the monthly payment: \n"))
-    months = math.ceil(loan_principal / monthly_payment)
-    print("It will take " + str(months)
-          + (" months to repay the loan" if months != 1 else " month to repay loan"))
+    interest = float(input("Enter the loan interest: \n")) / 100
+    nominal_rate = interest / 12
+    months = math.ceil(math.log(monthly_payment / (monthly_payment - nominal_rate * principal), nominal_rate + 1))
+    years = months // 12
+    print(f"It will take {years} years "
+          + (f"and {months - (years * 12)} months" if months % 12 != 0 else "")
+          + " to repay this loan!")
+elif option == "a":
+    principal = int(input("Enter the loan principal: \n"))
+    number_of_periods = int(input("Enter the number of periods: \n"))
+    interest = float(input("Enter the loan interest: \n")) / (12 * 100)
+    monthly_payment = principal \
+                      * (interest
+                         * (1 + interest) ** number_of_periods) / ((1 + interest) ** number_of_periods - 1)
+    print(f"Your monthly payment = {math.ceil(monthly_payment)}!")
 elif option == "p":
-    months = int(input("Enter the number of months: \n"))
-    monthly_payment = math.ceil(loan_principal / months)
-    last_payment = loan_principal - ((months - 1) * monthly_payment)
-    if last_payment == 0:
-        print("Your monthly payment = " + str(monthly_payment))
-    else:
-        print("Your monthly payment = " + str(monthly_payment)
-              + " and the last payment = " + str(math.ceil(last_payment)) + ".")
-
+    annuity_payment = float(input("Enter the annuity payment: \n"))
+    number_of_periods = int(input("Enter the number of periods: \n"))
+    interest = float(input("Enter the loan interest: \n")) / (12 * 100)
+    principal = annuity_payment \
+                / ((interest * (1 + interest) ** number_of_periods)
+                   / ((1 + interest) ** number_of_periods - 1))
+    print(f"Your loan principal = {math.floor(principal)}!")
